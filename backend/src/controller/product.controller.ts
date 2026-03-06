19609a9ed7a3e1c1a6d03f45ac0dd7e809c0e750
devkrsna <krishnagavali973@gmail.com>
2026-03-06 11:14:42 +0530

    feat: implement product creation logic and update product model

--- backend/src/controller/product.controller.ts
+++ backend/src/controller/product.controller.ts
@@ -1,5 +1,32 @@
 import type { Request, Response } from 'express';
 
-const addProduct = (req: Request, res: Response) => {};
+import Product from '@src/models/product.model';
+
+const addProduct = async (req: Request, res: Response) => {
+  const { name, description } = req.body;
+
+  if (!name || !description) {
+    return res
+      .status(400)
+      .json({ error: 'Name, description and category are required' });
+  }
+
+  try {
+    const createdProduct = await Product.create({
+      name,
+      description,
+      category: '',
+      seo_tags: [],
+      sustainability_attributes: [],
+    });
+
+    return res.status(201).json({
+      message: 'Product created successfully',
+      product: createdProduct,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to create product' });
+  }
+};
 
 export default addProduct;
--- backend/src/models/product.model.ts
+++ backend/src/models/product.model.ts
@@ -1,7 +1,17 @@
 import { model, Schema } from 'mongoose';
 
-const productSchema = new Schema(
+type product = {
+  _id: string;
+  name: string;
+  description: string;
+  category: string;
+  seo_tags: string[];
+  sustainability_attributes: string[];
+};
+
+const productSchema = new Schema<product>(
   {
+    _id: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
     category: { type: String, required: true },
@@ -14,3 +24,5 @@ const productSchema = new Schema(
 const Product = model('Product', productSchema);
 
 export default Product;
+
+export type { product };
